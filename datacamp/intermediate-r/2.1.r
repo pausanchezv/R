d36a72d13776a945888fb64d6a13419e9da47122
Pau Sanchez <pausanchez.admifin@gmail.com>
2018-01-01 11:23:25 +0100

    Add files via upload

---
+++ datacamp/intermediate-r/2.1.r
@@ -0,0 +1,12 @@
+
+# Initialize the speed variable
+speed <- 64
+
+# Code the while loop
+while (speed > 30) {
+  speed <- speed -7
+  print("Slow down!")
+}
+
+# Print out the speed variable
+speed
